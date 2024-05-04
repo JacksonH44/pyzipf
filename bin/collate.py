1354ebf07372d1dfe07796c2ec8754c3a8035f3c
Jackson Howe <jacksonhowe3@gmail.com>
2024-05-04 15:04:33 -0400

    Add verbose command line arg to collate.py

--- bin/collate.py
+++ bin/collate.py
@@ -24,6 +24,8 @@ def update_counts(reader, word_counts):
 
 def main(args):
     """Run the command line program."""
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
     word_counts = Counter()
     logging.info('Processing files...')
     for fname in args.infiles:
@@ -44,5 +46,8 @@ if __name__ == '__main__':
     parser.add_argument('-n', '--num',
                         type=int, default=None,
                         help='Output n most frequent words')
+    parser.add_argument('-v', '--verbose',
+                        action='store_true',
+                        help='Change logging level from default level to noisiest level')
     args = parser.parse_args()
     main(args)
